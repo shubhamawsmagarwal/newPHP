c8769a856841176a17f537c437f3b07ce97f2bee
EC2 Default User <ec2-user@ip-172-31-72-41.ec2.internal>
2020-06-30 22:35:15 +0000

    v2

--- phpdata/views/register.php
+++ phpdata/views/register.php
@@ -3,7 +3,7 @@
 <a href="/" class="btn btn-lg btn-success basicButtons">Home</a>
 <div class="container content">
     <form action="/register" method="post" class="text-center border border-light p-5" style="width:40%;">
-        <input type="username" name="username" class="form-control mb-4" placeholder="E-mail" required>
+        <input type="email" name="username" class="form-control mb-4" placeholder="E-mail" required>
         <input type="password" name="password" class="form-control" placeholder="Password" required>
         <button type="submit" class="btn btn-info my-4 btn-block">Register</button>
     </form>
